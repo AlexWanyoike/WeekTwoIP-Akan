bc0ff638fa7e8720247c43606fb8414a1c2b7f4d
AlexWanyoike <alex.wanyoike12@gmail.com>
2021-02-21 23:22:53 +0300

    All folder created tracked

--- js/script.js
+++ js/script.js
@@ -2,52 +2,49 @@ function getName(event){
     event.preventDefault()
     let date = document.getElementById('date').value
     let outputTag = document.getElementById('output')
-    //let myBirthday = new Date(date).value
     let myBirthday = new Date(date).toDateString()
-    let day = myBirthday.split( ' ' )
+    let day = myBirthday.split( ' ' )[0]
+    alert (day)
     let name = document.getElementById('name').value
     let gender = document.getElementById('gender').value  
-    let message = `Your were born on the ${date} and you are ${gender}` 
-    let akanname;
-    let femalenames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
-    let malenames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
-  
+    const femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
+    const maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
+    console.log(femaleNames);
+    console.log(maleNames);
+    let solution = document.getElementById('solution')
+     let akanName;
 
-    
-    if (gender ==='male' && day ==='Sun'){
-        akanname = malenames[0]
-   }else if (gender ==='male' && day ==='Mon'){
-        akanname = malenames[1]
-   }else if (gender ==='male' && day ==='Tue'){
-        akanname = malenames[2]
-   }else if (gender ==='male' && day ==='Wed'){
-        akanname = malenames[3]
-   }else if (gender ==='male' && day ==='Thu'){
-        akanname = malenames[4]
-   }else if (gender ==='male' && day ==='Fri'){
-        akanname = malenames[5]
-   }else if (gender ==='male' && day ==='Sat'){
-        akanname = malenames[6]
-   }else if (gender ==='female' && day ==='Sun'){
-        akanname = femalenames[0]
-   }else if (gender ==='female' && day ==='Mon'){
-        akanname = femalenames[1]
-   }else if (gender ==='female' && day ==='Tue'){
-        akanname = femalenames[2]
-   }else if (gender ==='female' && day ==='Wed'){
-        akanname = femalenames[3]
-   }else if (gender ==='female' && day ==='Thu'){
-        akanname = femalenames[4]
-   }else if (gender ==='female' && day ==='Fri'){
-        akanname = femalenames[5]
-   }else if (gender ==='female' && day ==='Sat'){
-        akanname = femalenames[6]
-   }
-   let message = `Your Akan name is ${akanname}`
-  
- let solution = document.getElementsById('solution')
-    solution.innerHTML= `Your Akan name would have been ${akanname}`
-  
+          if (gender=== 'male' && day==="Sun"){
+               akanName= maleNames[0]
+          }else if (gender=== 'male' && day==="Mon"){
+               akanName= maleNames[1]
+          }else if (gender=== 'male' && day==="Tue"){
+               akanName=maleNames[2]
+          }else if (gender=== 'male' && day==="Wed"){
+               akanName=(maleNames[3])
+          }else if (gender=== 'male' && day==="Thu"){
+               akanName=(maleNames[4])
+          }else if (gender=== 'male' && day==="Fri"){
+               akanName=(maleNames[5])
+          }else if (gender=== 'male' && day==="Sat"){
+               akanName=(maleNames[6])
+          }else if (gender=== 'female' && day==="Sun"){
+               akanName=(femaleNames[0])
+          }else if (gender=== 'female' && day==="Mon"){
+               akanName=(femaleNames[1])
+          }else if (gender=== 'female' && day==="Tue"){
+               akanName=(femaleNames[2])
+          }else if (gender=== 'female' && day==="Wed"){
+               aakanName=(femaleNames[3])
+          }else if (gender=== 'female' && day==="Thu"){
+               akanName=(femaleNames[4])
+          }else if (gender=== 'female' && day==="Fri"){
+               akanName=(femaleNames[5])
+          }else if (gender=== 'female' && day==="Sat"){
+               akanName=(femaleNames[6])
+          }    
+    let message = `Hey ${name} were born on the ${myBirthday} and you are ${gender}` 
+    solution.innerHTML= `Hey if you were Ghanian your Akan name would have been ${akanName}`
     outputTag.innerHTML = message
 
 }
