4de9590c46b7a7e04c66249eb1c58461e2054f59
AlexWanyoike <alex.wanyoike12@gmail.com>
2021-02-21 17:59:57 +0300

    Output that gives name, gender and birthdate

--- js/script.js
+++ js/script.js
@@ -2,10 +2,15 @@ function getName(event){
     event.preventDefault()
     let date = document.getElementById('date').value
     let outputTag = document.getElementById('output')
+    //let myBirthday = new Date(date).value
     let myBirthday = new Date(date).toDateString()
-    let day = myBirthday.split('')[]
-    let message =` Hey ${name} you were born on ${day}`
+    let day = myBirthday.split(' ')
+    let name = document.getElementById('name').value
+    let gender = document.getElementById('gender').value
+    let message =`Hey ${name} you were born on ${day} and you are ${gender}`
     outputTag.innerHTML = message
 }
+
 let femalenames = [Akosua,Adwoa,Abenaa,Akua,Yaa,Afua,Ama]
 let malenames = [Kwasi,Kwadwo,Kwabena,Kwaku,Yaw,Kofi,Kofi,Kwame]
+
