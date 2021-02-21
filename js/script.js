2ffee4e9e36b6cceaffa089cccc5fdd6e7e8e429
AlexWanyoike <alex.wanyoike12@gmail.com>
2021-02-21 13:20:46 +0300

    Output Form Input

--- js/script.js
+++ js/script.js
@@ -1,2 +1,8 @@
-console.log("Hey!")
-document.body.innerHTML="This is ok"
+function getName(event){
+    event.preventDefault()
+    let date = document.getElementById('date').value
+    let outputTag = document.getElementById('output')
+    let message = `Your were born on the ${date}`   
+    outputTag.innerHTML = message
+    console.log(date);
+}
