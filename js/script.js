92e76a2a6fae8ae8cd90f448780012fd81764f71
AlexWanyoike <alex.wanyoike12@gmail.com>
2021-02-21 14:06:49 +0300

    Input of Female and Male Names

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@ function getName(event){
     let date = document.getElementById('date').value
     let outputTag = document.getElementById('output')
     let myBirthday = new Date(date).toDateString()
-    let day = myBirthday.split('')
-    let message =`${day}`
+    let day = myBirthday.split('')[]
+    let message =` Hey ${name} you were born on ${day}`
     outputTag.innerHTML = message
 }
+let femalenames = [Akosua,Adwoa,Abenaa,Akua,Yaa,Afua,Ama]
+let malenames = [Kwasi,Kwadwo,Kwabena,Kwaku,Yaw,Kofi,Kofi,Kwame]
